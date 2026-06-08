76fac79792b02bee06fc01ed0a59a6bddde589ae
A1iHassan <ali012wkout@gmail.com>
2026-06-08 22:09:32 +0300

    an edit

--- Database/problems.sql
+++ Database/problems.sql
@@ -1,2 +1,2 @@
-# Write your MySQL query statement below
+# Write your MySQL query statement to get the second heighest salary of an employee (should return null if no second heighest is found)
 SELECT MAX(salary) AS SecondHighestSalary FROM Employee WHERE salary < (SELECT MAX(salary) FROM Employee);
